2248c536113bed38ba4e0ebcfe8ac08cc8c2e58c
npezza93 <npezza93@gmail.com>
2016-02-02 15:22:09 -0500

    Add job title to users

--- lib/absorb_api/user.rb
+++ lib/absorb_api/user.rb
@@ -2,7 +2,7 @@ module AbsorbApi
   class User < Base
     include Relations
 
-    attr_accessor :id, :department_id, :first_name, :middle_name, :last_name, :username, :password, :email_address, :cc_email_addresses, :language_id, :gender, :address, :address2, :city, :province_id, :country_id, :postal_code, :phone, :employee_number, :location, :job_title, :reference_number, :date_hired, :date_terminated, :notes, :custom_fields, :role_ids, :active_status, :is_learner, :is_admin, :is_instructor, :external_id, :supervisor_id, :decimal2, :string1, :decimal1, :string2, :decimal3
+    attr_accessor :id, :department_id, :first_name, :middle_name, :last_name, :username, :password, :email_address, :cc_email_addresses, :language_id, :gender, :address, :address2, :city, :province_id, :country_id, :postal_code, :phone, :employee_number, :location, :job_title, :reference_number, :date_hired, :date_terminated, :notes, :custom_fields, :role_ids, :active_status, :is_learner, :is_admin, :is_instructor, :external_id, :supervisor_id, :decimal2, :string1, :decimal1, :string2, :decimal3, :job_title
 
     has_many :courses
     has_many :enrollments, klass: :course_enrollment
