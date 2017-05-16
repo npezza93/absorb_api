c5cf6352ab2f6a98c215899c08c7f65f75cd003b
npezza93 <npezza93@gmail.com>
2017-05-16 19:48:06 -0400

    Cleanup Authorize class

--- lib/absorb_api/authorize.rb
+++ lib/absorb_api/authorize.rb
@@ -5,15 +5,29 @@ module AbsorbApi
     attr_reader :token
 
     def initialize
-      @token ||= Faraday.new(url: AbsorbApi.configuration.url) do |faraday|
-        faraday.request :url_encoded
-        faraday.response :logger
-        faraday.adapter :typhoeus
-      end.post do |req|
-        req.url "Authenticate", username: AbsorbApi.configuration.absorbuser, password: AbsorbApi.configuration.absorbpass, privateKey: AbsorbApi.configuration.absorbkey
+      @token ||= connection.post do |req|
+        req.url "Authenticate", payload
         req.headers["Content-Type"] = "application/json"
         req.headers["accept"] = "json"
       end.body.delete('\\"')
+    end
+
+    private
+
+    def connection
+      @connection ||= Faraday.new(url: AbsorbApi.configuration.url) do |faraday|
+        faraday.request :url_encoded
+        faraday.response :logger
+        faraday.adapter :typhoeus
+      end
+    end
+
+    def payload
+      {
+        username: AbsorbApi.configuration.absorbuser,
+        password: AbsorbApi.configuration.absorbpass,
+        privateKey: AbsorbApi.configuration.absorbkey
+      }
     end
   end
 end
